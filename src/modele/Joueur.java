4baa9ce5e5624c6e94cce7d2443744b70f181b4f
Chabroul <cheberli.iyed@gmail.com>
2018-06-05 00:18:44 +0200

    Bug interface graphique reglé !

--- src/modele/Joueur.java
+++ src/modele/Joueur.java
@@ -97,17 +97,19 @@ public class Joueur {
 
 	public Point coup(int jeu, PieceDomino piece, int indexDePieceDansLaMain, Table table, int posX, int posY,
 			int rotation, boolean centre, Point oldP) {
-		int x, y;
+		int x=1, y=1;
 		if (jeu == 0) {
 			this.getMain().get(indexDePieceDansLaMain).setCentre(centre);
 			this.getMain().get(indexDePieceDansLaMain).setRot(rotation);
 			if (!this.isCpu()) {
-				x = this.getPointJoueurHumain(table, piece, posX, posY).getX();
-				y = this.getPointJoueurHumain(table, piece, posX, posY).getY();
+				Point pp=this.getPointJoueurHumain(table, piece, posX, posY);
+				if(pp!=null){
+				x = pp.getX();
+				y = pp.getY();
 
 				System.out.println("x=" + x + "y= " + y);
-
-				if (table.coupValide(jeu, piece, new Point(x, y), new Point(0, 0))) {
+				}
+				if (table.coupValide(jeu, piece, new Point(x, y), new Point(posX, posY))) {
 					System.out.println("Joueur:coup valide en" + posX + posY + "avec la piece" + piece);
 					Point ptx = this.getPointJoueurHumain(table, piece, posX, posY);
 					table.getTable()[ptx.getX()][ptx.getY()] = piece;
@@ -116,6 +118,13 @@ public class Joueur {
 					for (int i = 0; i < table.getExtremite().size(); i++) {
 						if (table.getExtremite().get(i).getX() == x && table.getExtremite().get(i).getY() == y) {
 							table.getExtremite().remove(i);
+						}
+						if (!table.getTable()[posX][posY].isCentre()) {
+							table.getExtremite().remove(new Point(posX, posY));
+							table.getExtremite().remove(new Point(posX - 1, posY));
+							table.getExtremite().remove(new Point(posX + 1, posY));
+							table.getExtremite().remove(new Point(posX, posY + 1));
+							table.getExtremite().remove(new Point(posX, posY - 1));
 						}
 					}
 					if (centre) {
@@ -174,9 +183,8 @@ public class Joueur {
 										table.getExtremite().add(new Point(posX, posY - 1));
 								}
 
-							
 								table.getExtremite().remove(p);
-								
+
 								/*
 								 * 
 								 * 
@@ -208,67 +216,109 @@ public class Joueur {
 	}
 
 	public Point getPointJoueurHumain(Table table, PieceDomino piece, int posX, int posY) {
+		//
+		// int x = posX;
+		// int y = posY;
+		// System.out.println("getPointJoueurHumain");
+		// System.out.println("posX=" + posX);
+		// System.out.println("posY=" + posY);
+		// PieceDomino pieceTable = table.getTable()[posX][posY];
+		// System.out.println("piece " + piece.getRot() + "piece table" +
+		// pieceTable.getRot());
+		// System.out.println("piece x=" + piece.getX() + " y =
+		// "+piece.getY()+piece);
+		// System.out.println("piece table x=" + pieceTable.getX()+" y= " +
+		// pieceTable.getY()+pieceTable);
+		//
+		// if ((piece.getRot() == 1 && pieceTable.getRot() == 1 && piece.getX()
+		// == pieceTable.getY())
+		// || (piece.getRot() == 1 && pieceTable.getRot() == 0 && piece.getX()
+		// == pieceTable.getX())
+		// || (piece.getRot() == 1 && pieceTable.getRot() == 0 && piece.getX()
+		// == pieceTable.getY())
+		// || (piece.getRot() == 0 && pieceTable.getY() == 1 && piece.getX() ==
+		// piece.getY()
+		// && piece.getX() == pieceTable.getY())) {
+		// return new Point(x + 1, y);
+		// } else if ((piece.getRot() == 1 && pieceTable.getRot() == 1 &&
+		// piece.getY() == pieceTable.getX())
+		// || (piece.getRot() == 1 && pieceTable.getRot() == 0 && piece.getY()
+		// == pieceTable.getY())
+		// || (piece.getRot() == 1 && pieceTable.getRot() == 0 && piece.getY()
+		// == pieceTable.getX())
+		// || (piece.getRot() == 0 && pieceTable.getY() == 1 && piece.getX() ==
+		// piece.getY()
+		// && piece.getX() == pieceTable.getY())) {
+		// return new Point(x - 1, y);
+		// } else if ((piece.getRot() == 0 && pieceTable.getRot() == 0 &&
+		// piece.getX() == pieceTable.getY())
+		// || (piece.getRot() == 0 && pieceTable.getRot() == 1 &&
+		// pieceTable.getX() == pieceTable.getY()
+		// && piece.getX() == pieceTable.getY())
+		// || (piece.getRot() == 1 && pieceTable.getRot() == 0 && piece.getX()
+		// == piece.getY()
+		// && piece.getX() == pieceTable.getY())) {
+		// return new Point(x, y + 1);
+		// } else if ((piece.getRot() == 0 && pieceTable.getRot() == 0 &&
+		// piece.getY() == pieceTable.getX())
+		// || (piece.getRot() == 0 && pieceTable.getRot() == 1 &&
+		// pieceTable.getX() == pieceTable.getY()
+		// && piece.getY() == pieceTable.getX())
+		// || (piece.getRot() == 1 && pieceTable.getRot() == 0 && piece.getX()
+		// == piece.getY()
+		// && piece.getX() == pieceTable.getX())) {
+		// return new Point(x, y - 1);
+		// } else {
+		// return new Point(0, 0);
+		// }
 
-		int x = posX;
-		int y = posY;
-		System.out.println("getPointJoueurHumain");
-		System.out.println("posX=" + posX);
-		System.out.println("posY=" + posY);
-		PieceDomino pieceTable = table.getTable()[posX][posY];
-		System.out.println("piece " + piece.getRot() + "piece table" + pieceTable.getRot());
-		System.out.println("piece x=" + piece.getX() + " y = "+piece.getY()+piece);
-		System.out.println("piece table x=" + pieceTable.getX()+" y= " + pieceTable.getY()+pieceTable);
+		if (table.coupValide(0, piece, new Point(posX - 1, posY), new Point(posX, posY)))
+			return new Point(posX - 1, posY);
+		else if (table.coupValide(0, piece, new Point(posX + 1, posY), new Point(posX, posY)))
+			return new Point(posX + 1, posY);
+		else if (table.coupValide(0, piece, new Point(posX, posY + 1), new Point(posX, posY)))
+			return new Point(posX, posY + 1);
+		else if (table.coupValide(0, piece, new Point(posX, posY - 1), new Point(posX, posY)))
+			return new Point(posX, posY - 1);
+		return null;
 
-		if ((piece.getRot() == 1 && pieceTable.getRot() == 1 && piece.getX() == pieceTable.getY())
-				|| (piece.getRot() == 1 && pieceTable.getRot() == 0 && piece.getX() == pieceTable.getX())
-				|| (piece.getRot() == 1 && pieceTable.getRot() == 0 && piece.getX() == pieceTable.getY())
-				|| (piece.getRot() == 0 && pieceTable.getY() == 1 && piece.getX() == piece.getY()
-						&& piece.getX() == pieceTable.getY())) {
-			return new Point(x + 1, y);
-		} else if ((piece.getRot() == 1 && pieceTable.getRot() == 1 && piece.getY() == pieceTable.getX())
-				|| (piece.getRot() == 1 && pieceTable.getRot() == 0 && piece.getY() == pieceTable.getY())
-				|| (piece.getRot() == 1 && pieceTable.getRot() == 0 && piece.getY() == pieceTable.getX())
-				|| (piece.getRot() == 0 && pieceTable.getY() == 1 && piece.getX() == piece.getY()
-						&& piece.getX() == pieceTable.getY())) {
-			return new Point(x - 1, y);
-		} else if ((piece.getRot() == 0 && pieceTable.getRot() == 0 && piece.getX() == pieceTable.getY())
-				|| (piece.getRot() == 0 && pieceTable.getRot() == 1 && pieceTable.getX() == pieceTable.getY()
-						&& piece.getX() == pieceTable.getY())
-				|| (piece.getRot() == 1 && pieceTable.getRot() == 0 && piece.getX() == piece.getY()
-						&& piece.getX() == pieceTable.getY())) {
-			return new Point(x, y + 1);
-		} else if ((piece.getRot() == 0 && pieceTable.getRot() == 0 && piece.getY() == pieceTable.getX())
-				|| (piece.getRot() == 0 && pieceTable.getRot() == 1 && pieceTable.getX() == pieceTable.getY()
-						&& piece.getY() == pieceTable.getX())
-				|| (piece.getRot() == 1 && pieceTable.getRot() == 0 && piece.getX() == piece.getY()
-						&& piece.getX() == pieceTable.getX())) {
-			return new Point(x, y - 1);
-		} else {
-			return new Point(0, 0);
-		}
-
+		// System.out.println("x manuel");
+		// int x=new Scanner(System.in).nextInt();
+		// System.out.println("y manuel");
+		// int y=new Scanner(System.in).nextInt();
+		// return new Point(x,y);
 	}
 
 	public boolean nePeutPasJouer(int jeu, Table table) {
 		if (jeu == 0) {
 			for (Point p : table.getExtremite()) {
-				for (Piece piece : this.getAllPiecesSwipes()) {
-					if (table.getTable()[p.getX()][p.getY() - 1] != null
-							&& piece.getX() == table.getTable()[p.getX()][p.getY() - 1].getY()) {
-						return false;
-					} else if (table.getTable()[p.getX()][p.getY() + 1] != null
-							&& piece.getY() == table.getTable()[p.getX()][p.getY() + 1].getX()) {
-						return false;
-					} else if (table.getTable()[p.getX() + 1][p.getY()] != null
-							&& piece.getX() == table.getTable()[p.getX() + 1][p.getY()].getY()) {
-						return false;
-					} else if (table.getTable()[p.getX() - 1][p.getY()] != null
-							&& piece.getY() == table.getTable()[p.getX() - 1][p.getY()].getX()) {
+				for (PieceDomino piece : this.getAllPiecesSwipes()) {
+					if (table.coupValide(jeu, piece, p, new Point(0, 0))) {
 						return false;
 					}
+					// if (table.getTable()[p.getX()][p.getY() - 1] != null
+					// && piece.getX() == table.getTable()[p.getX()][p.getY() -
+					// 1].getY()) {
+					// return false;
+					// } else if (table.getTable()[p.getX()][p.getY() + 1] !=
+					// null
+					// && piece.getY() == table.getTable()[p.getX()][p.getY() +
+					// 1].getX()) {
+					// return false;
+					// } else if (table.getTable()[p.getX() + 1][p.getY()] !=
+					// null
+					// && piece.getX() == table.getTable()[p.getX() +
+					// 1][p.getY()].getY()) {
+					// return false;
+					// } else if (table.getTable()[p.getX() - 1][p.getY()] !=
+					// null
+					// && piece.getY() == table.getTable()[p.getX() -
+					// 1][p.getY()].getX()) {
+					// return false;
+					// }
 				}
 			}
-
+			System.err.println("*** "+this.getNom()+" ne peu pas jouer");
 			return true;
 		} else {
 			// TODO trio
